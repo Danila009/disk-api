49721637f452ad890662d8aa58951f2769606a8a
ISPr24-39_BeluakovDS <dan.bel.89@bk.ru>
2023-06-27 03:09:32 +0400

    login

--- src/main/java/ru/disk/Disk/features/user/UserController.java
+++ src/main/java/ru/disk/Disk/features/user/UserController.java
@@ -1,5 +1,6 @@
 package ru.disk.Disk.features.user;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.disk.Disk.features.user.dto.JwtRequestDto;
+import ru.disk.Disk.features.user.dto.JwtResponseDto;
+import ru.disk.Disk.features.user.dto.RefreshJwtRequestDto;
 import ru.disk.Disk.features.user.dto.RegisterDto;
 
 @RestController
@@ -18,9 +22,28 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("register")
+    @Operation(summary = "register user")
     public ResponseEntity<String> register(@RequestBody RegisterDto dto) {
         userService.register(dto);
 
         return ResponseEntity.ok("success");
+    }
+
+    @PostMapping("login")
+    @Operation(summary = "login user")
+    public JwtResponseDto login(@RequestBody JwtRequestDto dto) {
+        return userService.login(dto);
+    }
+
+    @PostMapping("token")
+    @Operation(summary = "get new access token")
+    public JwtResponseDto getNewAccessToken(@RequestBody RefreshJwtRequestDto request) {
+        return userService.getAccessToken(request.getRefreshToken());
+    }
+
+    @PostMapping("refresh")
+    @Operation(summary = "get new refresh token")
+    public JwtResponseDto getNewRefreshToken(@RequestBody RefreshJwtRequestDto request) {
+        return userService.refresh(request.getRefreshToken());
     }
 }
